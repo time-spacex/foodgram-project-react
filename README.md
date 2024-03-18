1abfd7dce88b900a96de302f3f00b60067bf68d8
Роман Клешнев <time-space@yandex.ru>
2024-03-18 18:23:51 +0300

    Add load_tags manage.py command

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # praktikum_new_diplom
 
-скрипт для автовыгрузки ингредиентов:
+команда для загрузки ингредиентов:
 python manage.py load_ingredients ../../data/ingredients.csv
+
+команда для загрузки тегов:
+python manage.py load_tags ../../data/tags.csv
--- backend/foodgram_backend/recipes/management/commands/load_tags.py
+++ backend/foodgram_backend/recipes/management/commands/load_tags.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 import os
 import csv
 
-from recipes.models import Ingredient
+from recipes.models import Tag
 
 
 class Command(BaseCommand):
@@ -16,10 +16,11 @@ class Command(BaseCommand):
                 with open(csv_file_path, 'r', encoding='utf-8') as file:
                     reader = csv.reader(file)
                     for row in reader:
-                        Ingredient.objects.create(
+                        Tag.objects.create(
                             name=row[0],
-                            measurement_unit=row[1]
+                            color=row[1],
+                            slug=row[2]
                         )
-                    self.stdout.write(self.style.SUCCESS(f'Ingredients in database successfuly created'))
+                    self.stdout.write(self.style.SUCCESS(f'Tags in database successfuly created'))
             else:
                 self.stderr.write(self.style.ERROR(f'File does not exist: {csv_file_path}'))
