f14ece543968f9e1bdb88b4b5fe2edd89e4f64a2
Роман Клешнев <time-space@yandex.ru>
2024-03-21 17:18:56 +0300

    Add load_users django management command

---
+++ backend/foodgram_backend/users/management/commands/load_users.py
@@ -0,0 +1,32 @@
+import os
+import csv
+
+from django.core.management.base import BaseCommand
+from rest_framework.authtoken.models import Token
+
+from users.models import CustomUser
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        for csv_file_path in options['csv_file']:
+            if os.path.exists(csv_file_path):
+                with open(csv_file_path, 'r', encoding='utf-8') as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        user = CustomUser.objects.create_user(
+                            username=row[0],
+                            first_name=row[1],
+                            last_name=row[2],
+                            email=row[3],
+                            password=row[4],
+                            is_admin=(row[5] == 'True')
+                        )
+                        Token.objects.get_or_create(user=user)
+                    self.stdout.write(self.style.SUCCESS(f'Users in database successfuly created'))
+            else:
+                self.stderr.write(self.style.ERROR(f'File does not exist: {csv_file_path}'))
