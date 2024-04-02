2288742497a5a2759e30e13213cb2d03c0f9a4d8
Роман Клешнев <time-space@yandex.ru>
2024-04-02 16:59:20 +0300

    fix github action

---
+++ backend/setup_script.sh
@@ -0,0 +1,10 @@
+cd foodgram_backend
+python manage.py migrate
+python manage.py collectstatic
+cp -r /app/foodgram_backend/collected_static/. /backend_static/static/
+python manage.py load_ingredients ../data/ingredients.csv
+python manage.py load_tags ../data/tags.csv
+python manage.py load_users ../data/users.csv
+python manage.py load_recipes ../data/recipes.csv
+curl -L https://disk.yandex.ru/d/PWF8KxVb2BbMFg/download -o /foodgram_backend/media.zip
+unzip -o /foodgram_backend/media.zip -d /app/foodgram_backend/media/
