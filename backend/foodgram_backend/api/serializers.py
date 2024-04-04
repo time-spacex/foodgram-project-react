b40e013355b8076e772e0235824e196f5d415a2a
Роман Клешнев <time-space@yandex.ru>
2024-04-04 11:26:31 +0300

    fix base64imageField in serializers

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -1,21 +1,9 @@
-import base64
-
-from django.core.files.base import ContentFile
 from django.core.mail import send_mail
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
 from users.models import CustomUser, Subscription
 from recipes.models import Recipe, Tag, Ingredient, IngredientsInRecipe
-
-
-class Base64ImageField(serializers.ImageField):
-    """Класс для получения изборажения."""
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
 
 
 class SignUpSerializer(serializers.ModelSerializer):
